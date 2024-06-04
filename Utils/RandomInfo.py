9acd7cee1f2e1e474b8b7aa3b1fdcc536fc6c009
lluc.paret <lluc.paret@estudiantat.upc.edu>
2024-06-05 00:06:12 +0200

    randomi

--- Utils/RandomInfo.py
+++ Utils/RandomInfo.py
@@ -1,12 +1,19 @@
 import random
 import string
-
+from datetime import datetime, timedelta
 import requests
 from rdflib import Graph, Literal, RDF, URIRef, Namespace
 from rdflib.namespace import XSD
 
 # Crear un grafo RDF
 g = Graph()
+
+mss_cnt = 899
+
+def get_count():
+    global mss_cnt
+    mss_cnt += 1
+    return mss_cnt
 
 # Definir la ontología y sus namespaces
 NS = Namespace("http://www.semanticweb.org/nilde/ontologies/2024/4/")
@@ -21,8 +28,8 @@ brands3 = ["Apple", "HP", "Lenovo"]
 centros_logisticos = ["http://www.semanticweb.org/ecsdi/ontologies/2024/4/Banyoles",
                       "http://www.semanticweb.org/ecsdi/ontologies/2024/4/Barcelona",
                       "http://www.semanticweb.org/ecsdi/ontologies/2024/4/Tarragona",
-                      "http://www.semanticweb.org/ecsdi/ontologies/2024/4/Valencia",
-                      "http://www.semanticweb.org/ecsdi/ontologies/2024/4/Zaragoza"]
+                      "http://www.semanticweb.org/ecsdi/ontologies/2024/Valencia",
+                      "http://www.semanticweb.org/ecsdi/ontologies/2024/Zaragoza"]
 
 venedors_externs = {
     "Nike": "ESBN00909191",
@@ -30,34 +37,19 @@ venedors_externs = {
     "Zara": "ESBN91120302102"
 }
 
+transportistas = ["Transportista1", "Transportista2", "Transportista3"]
+
 # Función para generar un ID único
 def generate_id():
     return f"P{random.randint(1000, 9999)}"
 
-
 def random_name(prefix, size=6, chars=string.ascii_uppercase + string.digits):
-    """
-    Genera un nombre aleatorio a partir de un prefijo, una longitud y una lista con los caracteres a usar
-    en el nombre
-    :param prefix:
-    :param size:
-    :param chars:
-    :return:
-    """
     return prefix + '_' + ''.join(random.choice(chars) for _ in range(size))
 
-
-if __name__ == '__main__':
-    # Crear instancias de vendedores externos
-    for venedor, compte in venedors_externs.items():
-        venedor_uri = URIRef(NS[venedor])
-        g.add((venedor_uri, RDF.type, NS.VenedorExtern))
-        g.add((venedor_uri, NS.Nom, Literal(venedor, datatype=XSD.string)))
-        g.add((venedor_uri, NS.CompteBancari, Literal(compte, datatype=XSD.string)))
-
-    # Crear instancias de productos aleatorios
+def generar_productos_aleatorios(g, num_productos=60):
+    productos = []
     for cat in categories:
-        for _ in range(20):
+        for _ in range(num_productos // len(categories)):
             product_id = generate_id()
             category = cat
             valoracion = round(random.uniform(1, 5), 2)
@@ -88,8 +80,16 @@ if __name__ == '__main__':
             g.add((product, NS.Preu, Literal(price, datatype=XSD.float)))
             g.add((product, NS.Valoracio, Literal(valoracion, datatype=XSD.float)))
             g.add((product, NS.Empresa, Literal("ECDI", datatype=XSD.string)))
-
-            # Añadir el producto a un centro logístico
+            productos.append({
+                'ID': product_id,
+                'Nom': name,
+                'Preu': price,
+                'Pes': weight,
+                'Categoria': category,
+                'Marca': brand,
+                'Valoracio': valoracion,
+                'Empresa': "ECDI"
+            })
 
             ncentres = int(random.uniform(1, 5))
             numeros_disponibles = list(range(1, 5))
@@ -98,25 +98,75 @@ if __name__ == '__main__':
                 centro_logistico = centros_logisticos[num]
                 centro = URIRef(centro_logistico)
                 g.add((product, NS.ProductesCentreLogistic, centro))
-    # Guardar el grafo en formato RDF/XML
+
+    return productos
+
+def generar_comandas_aleatorias(g, productos, dni_cliente='41', num_comandas=5):
+    for i in range(num_comandas):
+        comanda_id = 'Comanda' + str(get_count())
+        ciutat = random.choice(['Barcelona', 'Madrid', 'Valencia', 'Sevilla', 'Zaragoza'])
+        preu_total = random.uniform(100, 1000)
+        prioritat = random.choice([1, 2, 3])
+        credit_card = ''.join(random.choices('0123456789', k=16))
+
+        products = random.sample(productos, 4)
+
+        comanda = URIRef(NS[comanda_id])
+        g.add((comanda, RDF.type, NS.Comanda))
+        g.add((comanda, NS.ID, Literal(comanda_id, datatype=XSD.string)))
+        g.add((comanda, NS.Ciutat, Literal(ciutat, datatype=XSD.string)))
+        g.add((comanda, NS.Client, URIRef(NS[dni_cliente])))
+        g.add((comanda, NS.PreuTotal, Literal(preu_total, datatype=XSD.float)))
+        g.add((comanda, NS.Prioritat, Literal(prioritat, datatype=XSD.integer)))
+        g.add((comanda, NS.TargetaCredit, Literal(credit_card, datatype=XSD.string)))
+
+        for producte in products:
+            producte_comanda_id = f"{comanda_id}_ProducteComanda_{producte['ID']}"
+            producte_comanda_uri = URIRef(NS[producte_comanda_id])
+
+            transportista = random.choice(transportistas)
+            fecha_transporte = (datetime.now() - timedelta(days=random.randint(0, 60))).date()
+
+            g.add((producte_comanda_uri, RDF.type, NS.ProducteComanda))
+            g.add((producte_comanda_uri, NS.Nom, Literal(producte['Nom'], datatype=XSD.string)))
+            g.add((producte_comanda_uri, NS.Preu, Literal(producte['Preu'], datatype=XSD.float)))
+            g.add((producte_comanda_uri, NS.Data, Literal(fecha_transporte, datatype=XSD.date)))
+            g.add((producte_comanda_uri, NS.Pagat, Literal(random.choice([True, False]), datatype=XSD.boolean)))
+            g.add((producte_comanda_uri, NS.Enviat, Literal(random.choice([True, False]), datatype=XSD.boolean)))
+            g.add((producte_comanda_uri, NS.Retornat, Literal("Pendiente", datatype=XSD.string)))
+            g.add((producte_comanda_uri, NS.TransportistaProducte, Literal(transportista, datatype=XSD.string)))
+            g.add((producte_comanda_uri, NS.Empresa, Literal(producte['Empresa'], datatype=XSD.string)))
+            g.add((producte_comanda_uri, NS.Valoracio, Literal(producte['Valoracio'], datatype=XSD.float)))
+            g.add((producte_comanda_uri, NS.Pes, Literal(producte['Pes'], datatype=XSD.float)))
+            g.add((producte_comanda_uri, NS.Categoria, Literal(producte['Categoria'], datatype=XSD.string)))
+            g.add((producte_comanda_uri, NS.Marca, Literal(producte['Marca'], datatype=XSD.string)))
+            g.add((comanda, NS.ProductesComanda, producte_comanda_uri))
+
+
+if __name__ == '__main__':
+    for venedor, compte in venedors_externs.items():
+        venedor_uri = URIRef(NS[venedor])
+        g.add((venedor_uri, RDF.type, NS.VenedorExtern))
+        g.add((venedor_uri, NS.Nom, Literal(venedor, datatype=XSD.string)))
+        g.add((venedor_uri, NS.CompteBancari, Literal(compte, datatype=XSD.string)))
+
+    productos = generar_productos_aleatorios(g, num_productos=60)
+    generar_comandas_aleatorias(g, productos, dni_cliente='41', num_comandas=10)
+
     g.serialize(destination="productos.rdf", format="xml")
 
     print("Se han generado instancias de productos aleatorios y se han guardado en 'productos.rdf'")
-    # Serializar el grafo a formato RDF/XML
+
     rdf_xml_data = g.serialize(format='xml')
 
-    # URL del endpoint de Fuseki (cambia esto por la URL de tu instancia de Fuseki y el nombre de tu dataset)
-    fuseki_url = 'http://localhost:3030/ONTO/data'  # Cambia 'dataset' por el nombre de tu dataset
+    fuseki_url = 'http://localhost:3030/ONTO/data'
 
-    # Cabeceras para la solicitud
     headers = {
-        'Content-Type': 'application/rdf+xml'  # Cambiado a 'application/rdf+xml'
+        'Content-Type': 'application/rdf+xml'
     }
 
-    # Enviamos los datos a Fuseki
     response = requests.post(fuseki_url, data=rdf_xml_data, headers=headers)
 
-    # Verificamos la respuesta
     if response.status_code == 200:
         print('Datos subidos exitosamente a Fuseki')
     else:
